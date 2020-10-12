0bbf524c3face18e3736f9aac1e7f15bba7e0c2a
Azatalapov <alapov.azater@mail.ru>
2020-10-13 00:45:18 +0300

    Задание 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        in.useLocale(Locale.US);
+        float a = 3;
+        float b = 8;
+        float x = in.nextFloat();
+        boolean y = (x>=3 && x<=8);
+        System.out.println(y);
+
+
+    }
+}
+
